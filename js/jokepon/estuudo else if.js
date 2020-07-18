93393af3c3cc6467de1f90666451142b960180db
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-07-18 20:55:07 -0300

    Create estuudo else if.js

---
+++ js/jokepon/estuudo else if.js
@@ -0,0 +1,19 @@
+ if (condicao){
+// condição verdadeira
+}
+
+if (condicao) {
+    // condição verdadeira
+} else {
+    // condição falsa
+}
+
+if (condicao 1) {
+    // condicao 1 verdadeira
+} else if (condicao 2){
+    // condincao 2 verdadeira    
+}else if (condicao 3){
+    // condicao 3 verdadeira
+} else{
+    // caso nenhuma condição seja verdadeira
+}
