5d52f030b6f620b76f360a4b470d07cf7881d2e9
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-07-17 22:05:42 -0300

    Create hellofunction.js

---
+++ js/simpleFuncion/hellofunction.js
@@ -0,0 +1,11 @@
+/**
+ * JS - Aula4: Vinculando um documento JS
+ * @author Edivan Carvalho
+ */
+
+ function hello(){
+     var nome;
+     nome = prompt("Qual é o seu nome?");
+     alert("Hello " + nome);
+     
+ }  
