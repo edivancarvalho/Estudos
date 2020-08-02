2f62e84c753ee0cc89560375b53d23dc10ff38a7
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-08-02 01:18:21 -0300

    Create InstalaçãoDocker.sh

---
+++ Docker/InstalaçãoDocker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#-----------HEADER-------------------------------------------------------------|
+# AUTOR             : Edivan Carvalho <edivancarvalho2008@hotmail.com>
+# HOMEPAGE          : https://github.com/edivancarvalho
+# DATA DE CRIAÇÃO   : 02/08/2020 às 01:37 
+# PROGRAMA          : SO - Fedora 31
+# VERSÃO            : 1.0.0
+# LICENÇA           : MIT
+# PEQUENA DESCRIÇÃO : Instalação de Feramentas de Desenvolvimento 
+#
+# CHANGELOG :
+#
+#------------------------------------------------------------------------------|
+# informação sobre todo o sistema;
+inxi -SCF
+
+# XXX - Codigo em processo.
+echo "Codigo em processo de implementação!!!"
