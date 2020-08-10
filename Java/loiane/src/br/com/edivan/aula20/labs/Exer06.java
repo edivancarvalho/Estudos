c51468ffaf3681ae837d5989fd0080055a14f348
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-08-10 10:44:46 -0300

    Update Exer06.java

--- Java/loiane/src/br/com/edivan/aula20/labs/Exer06.java
+++ Java/loiane/src/br/com/edivan/aula20/labs/Exer06.java
@@ -1,3 +1,13 @@
+/**
+ * Faça um programa para jogar o jogo da velha. O programa deve
+permitir que dois jogadores façam uma partida do jogo da velha,
+usando o computador para ver o tabuleiro. Cada jogador vai
+alternadamente informando a posição onde deseja colocar a sua
+peça (‘O’ ou ‘X’). O programa deve impedir jogadas inválidas e
+determinar automaticamente quando o jogo terminou e quem foi o
+vencedor (jogador1 ou jogador2). A cada nova jogada, o programa
+deve atualizar a situação do tabuleiro na tela.
+ */
 package br.com.edivan.aula20.labs;
 
 import java.util.Scanner;
