26f62d0e645053928b5f888fd6f19e6da29a8d93
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-08-10 09:49:35 -0300

    Create Exer06.java

---
+++ Java/loiane/src/br/com/edivan/aula20/labs/Exer06.java
@@ -0,0 +1,23 @@
+package br.com.edivan.aula20.labs;
+
+import java.util.Scanner;
+
+/**
+ * * @author edivan
+ */
+public class Exer06 {
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        
+        char [][] jogoVelha = new char [3][3];
+        
+        System.out.println("Jogaro 1 = X");
+        System.out.println("Jogaro 2 = O");
+        
+        boolean ganhou = false;
+        while (!ganhou){
+            
+        }
+    }
+}
