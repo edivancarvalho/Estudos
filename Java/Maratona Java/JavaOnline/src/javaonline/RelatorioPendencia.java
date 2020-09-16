2c89161decf83119c1d3c56bc82851170e008f7e
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-09-16 17:12:14 -0300

    Create RelatorioPendencia.java

---
+++ Java/Maratona Java/JavaOnline/src/javaonline/RelatorioPendencia.java
@@ -0,0 +1,20 @@
+
+package javaonline;
+
+/**
+ *
+ * @author edivan
+ */
+public class RelatorioPendencia {
+    public static void main(String[] args) {
+        
+        String nome = "Edivan";
+        String endereco = "rua sp";
+        String telefone = "2345245234";
+        
+        System.out.println("O " + nome + " enredeço " + endereco + " Telefone" + telefone);
+        
+        
+    }
+    
+}
