36bc0cffed489ed26ee949daaa019381644bef00
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-09-16 17:57:04 -0300

    Update Operators.java

--- Java/Maratona Java/JavaOnline/src/javaonline/Operators.java
+++ Java/Maratona Java/JavaOnline/src/javaonline/Operators.java
@@ -10,6 +10,15 @@ public class Operators {
         
         int numero1 = 10;
         int numero2 = 20;
+        int soma = numero1+numero2;
+        
+        System.out.println("A soma: " + soma );
+        
+        System.out.println("A soma: " + (numero1 + numero2));
+        System.out.println("subtração: " + (numero1 - numero2));
+        System.out.println("Multiplicaçao: " + (numero1 * numero2));
+        System.out.println("A divisão: " + (numero1 / numero2)); // 10/20 = 0.5
+        System.out.println("A divisão: " + (numero2 / numero1)); // 20/10 = 2
     }
     
 }
