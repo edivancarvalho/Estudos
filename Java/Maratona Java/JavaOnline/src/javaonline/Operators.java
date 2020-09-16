efc0e6135071920869624b82f1575a2e407b0bbc
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-09-16 17:49:33 -0300

    Create Operators.java

---
+++ Java/Maratona Java/JavaOnline/src/javaonline/Operators.java
@@ -0,0 +1,15 @@
+
+package javaonline;
+
+/**
+ *
+ * @author edivan
+ */
+public class Operators {
+    public static void main(String[] args) {
+        
+        int numero1 = 10;
+        int numero2 = 20;
+    }
+    
+}
