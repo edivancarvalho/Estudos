d4e095c2027fc050c75cafc25b8456a49d5806b8
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-09-16 17:49:37 -0300

    Create Sexo.java

---
+++ Java/Maratona Java/JavaOnline/src/javaonline/Sexo.java
@@ -0,0 +1,24 @@
+package javaonline;
+
+/**
+ *
+ * @author edivan
+ */
+public class Sexo {
+    public static void main(String[] args) {
+        
+        String nome = "Edivan";
+        Double salario = 4500.00;
+        char sexo = 'M';
+        int idade = 30;
+        String estadoCivil = "Solteiro";
+        
+        System.out.println("Nome: " + nome);
+        System.out.println("Salário: "+salario);
+        System.out.println("Sexo: "+ sexo);
+        System.out.println("Idade " + idade);
+        System.out.println("Estado Civil: "+ estadoCivil);
+        
+    }
+    
+}
