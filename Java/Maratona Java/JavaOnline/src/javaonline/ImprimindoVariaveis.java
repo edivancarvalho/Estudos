6529024c608a6642efa5fff2bd774c4fcf160be7
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-09-16 17:12:06 -0300

    Create ImprimindoVariaveis.java

---
+++ Java/Maratona Java/JavaOnline/src/javaonline/ImprimindoVariaveis.java
@@ -0,0 +1,24 @@
+
+package javaonline;
+
+/**
+ *
+ * @author edivan
+ */
+public class ImprimindoVariaveis {
+    public static void main(String[] args) {
+        int idade = 10;
+        double salarioDouble = 3000;
+        float slarioFloat = 3000;
+        byte idadeByte = 12;
+        short idadeshort = 32767;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        long nuermoGrande = 1000L;
+        char caractere  = 'A';
+        char caractere01 = '\u0041';
+        String nome = "Edivan";
+        
+        System.out.println(nome + " de idade " + idade);
+    }
+}
