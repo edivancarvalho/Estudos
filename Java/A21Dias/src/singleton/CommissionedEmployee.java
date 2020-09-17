ec05227f76048f3222802c78dd93d58fbfd1f309
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-09-17 09:19:38 -0300

    Create CommissionedEmployee.java

---
+++ Java/A21Dias/src/singleton/CommissionedEmployee.java
@@ -0,0 +1,33 @@
+package singleton;
+
+/**
+ * pagina 172
+ * @author edivan
+ */
+public class CommissionedEmployee extends Employee{
+    
+    private double commission;   // o custo por unidade
+    private int units;          // controla o número de unidades vendidas
+    
+    public CommissionedEmployee(String first_name, String last_name, double wage, double commission){
+        super(first_name, last_name, wage);        
+        // chama o construtor original para inicializar corretamente
+        this.commission = commission;
+    }
+    
+    public double calculatePay(){
+        return getWage() + (commission * units);
+    }
+    
+    public void addSales( int units){
+        this.units = this.units + units;
+    }
+    
+    public int getSales(){
+        return units;
+    }
+    
+    public void resetSales(){
+        units = 0;
+    }
+}
