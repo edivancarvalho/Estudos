07117ae36c88a65676cc27a2a7c14c47865c184e
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-09-18 09:51:36 -0300

    Create OperadoresComparacao.java

---
+++ Java/Maratona Java/JavaOnline/src/javaonline/OperadoresComparacao.java
@@ -0,0 +1,17 @@
+package javaonline;
+/**
+ *
+ * @author edivan
+ */
+public class OperadoresComparacao {
+    public static void main(String[] args) {
+        
+        boolean dezMaiorquevinte = 10 > 20;
+        boolean dezMenoQuevinte = 10 < 20;
+        System.out.println(dezMaiorquevinte);
+        System.out.println(dezMenoQuevinte);
+        
+        System.out.println(5 == 5);
+        System.out.println(5 != 5);
+    }
+}
