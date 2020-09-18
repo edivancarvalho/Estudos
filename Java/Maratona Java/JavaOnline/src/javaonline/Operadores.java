b4dc73e604792adbe6edb84fbe0e8bfe81f1522d
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-09-18 09:51:30 -0300

    Create Operadores.java

---
+++ Java/Maratona Java/JavaOnline/src/javaonline/Operadores.java
@@ -0,0 +1,21 @@
+package javaonline;
+
+/**
+ *
+ * @author edivan
+ */
+public class Operadores {
+    public static void main(String[] args) {
+        
+        int num = 20;
+        long num2 = 20000L;
+        
+        double numDouble = 10d;
+        float numFloat = 20f;
+        
+        numFloat = (float)numDouble;
+        System.out.println("resto da divisao: " + (20%2));
+        System.out.println(numDouble);
+    }
+    
+}
