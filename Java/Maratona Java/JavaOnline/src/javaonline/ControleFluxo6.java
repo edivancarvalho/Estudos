e9d2a628b1bc4bd7274eb692d4a924189085755c
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-09-18 11:22:50 -0300

    Create ControleFluxo6.java

---
+++ Java/Maratona Java/JavaOnline/src/javaonline/ControleFluxo6.java
@@ -0,0 +1,26 @@
+package javaonline;
+
+/**
+ *
+ * @author edivan
+ */
+public class ControleFluxo6 {
+    // usando o continue;
+    public static void main(String[] args) {
+        // dado um valor de um carro, descubra quantas vezes ele pode ser parcelado
+        // porem as parcelas noa poder ser menores do que 1000;
+
+        double valorTotal = 30000;
+        
+        for (int parcela = (int) valorTotal; parcela >=1; parcela--){
+            double valorParcela = valorTotal / parcela;
+            if (valorParcela <= 1000){
+            continue;
+                // o codigo do sout não será executando nunca com o continue;
+                //System.out.println(" vaiiii");
+            }
+            System.out.println("Parcela" + parcela + " R$ " + valorParcela); 
+        }
+    }
+    
+}
