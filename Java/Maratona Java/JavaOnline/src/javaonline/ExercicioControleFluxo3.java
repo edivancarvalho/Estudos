a783d0753d1ab38dbf02171a8baef9baedbc4d75
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-09-18 11:23:00 -0300

    Create ExercicioControleFluxo3.java

---
+++ Java/Maratona Java/JavaOnline/src/javaonline/ExercicioControleFluxo3.java
@@ -0,0 +1,25 @@
+package javaonline;
+
+/**
+ *
+ * @author edivan
+ */
+public class ExercicioControleFluxo3 {
+
+    public static void main(String[] args) {
+// Imprima todos os numeros pares de 0 até 100000;
+
+        int valor = 100;
+
+        for (int i = 0; i <= valor; i++) {
+            if (i % 2 == 0) { // impar mudar [ i % 2 != 0 ]
+                System.out.println("Par: " + i);
+            }else{
+                System.out.println("Impar: " + i);
+            }
+        }
+        
+        
+    }
+
+}
