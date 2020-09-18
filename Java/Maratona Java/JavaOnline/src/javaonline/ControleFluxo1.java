d605bd9707847f9da808fece85ae4ae14ad0dac4
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-09-18 09:51:11 -0300

    Create ControleFluxo1.java

---
+++ Java/Maratona Java/JavaOnline/src/javaonline/ControleFluxo1.java
@@ -0,0 +1,44 @@
+package javaonline;
+
+/**
+ *
+ * @author edivan
+ */
+public class ControleFluxo1 {
+    public static void main(String[] args) {
+        
+        int idade = 20;
+        float salario = 2000;
+        boolean c = false;
+        boolean d = false;
+        
+        String categoria = "";
+        
+        if (c = true) {
+            System.out.println("Imprimir verdadeiro");
+            
+        }
+        if (d == true) {
+            System.out.println("c é falso");
+            
+        }
+        
+        if (idade > 18){
+            System.out.println("Entrou no IF");
+        }else{
+            System.out.println("Nao pode entra no clube");
+        }
+        //-------------------------------------------------
+        if (idade > 15){
+            categoria = "infantil";
+        }
+        else if (idade >=15 && idade <18){
+            categoria = "Juvenil";
+        }
+        else{
+            categoria = "Adulto";
+        }
+        System.out.println(categoria);
+        
+    }
+}
