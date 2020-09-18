5285d498cc940f9434015e20b1614df883bd81ab
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-09-18 09:51:17 -0300

    Create ControleFluxo2.java

---
+++ Java/Maratona Java/JavaOnline/src/javaonline/ControleFluxo2.java
@@ -0,0 +1,28 @@
+package javaonline;
+
+/**
+ * Operado Ternario
+ *
+ * @author edivan
+ */
+public class ControleFluxo2 {
+
+    public static void main(String[] args) {
+        
+        int idade = 20;
+        String status = "";
+        String status2 = "";
+        
+        if (idade < 18){
+            status = " não Adulto";
+        }else{
+            status = "Adulto";
+        }
+        System.out.println(status);
+        
+        // mesmo codigo usando o ternario
+        status2 = idade < 18 ? "Não Adulto" : "Adulto"; // sempre dever seguir a logica do booleano;
+        System.out.println(status2);
+    }
+
+}
