bda991b9738fff212aa23f006ee9b5742af06067
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-09-18 21:10:55 -0300

    Create Array1.java

---
+++ Java/Maratona Java/JavaOnline/src/br/com/abc/introducao/Array1.java
@@ -0,0 +1,21 @@
+package br.com.abc.introducao;
+
+/**
+ *
+ * @author edivan
+ */
+public class Array1 {
+
+    public static void main(String[] args) {
+
+        int[] idades = new int[3];
+        idades[0] = 15;
+        idades[1] = 20;
+        idades[2] = 12;
+        
+        System.out.println("Idade 1: " + idades[0]);
+        System.out.println("Idade 2: " + idades[1]);
+        System.out.println("Idade 3: " + idades[2]);
+    }
+
+}
