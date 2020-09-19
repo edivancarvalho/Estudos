7eba2fd868e1abee8056a9b407348a8e95177aa9
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-09-18 22:30:31 -0300

    Create Professor.java

---
+++ Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/introducaoclasses/classes/Professor.java
@@ -0,0 +1,21 @@
+package br.com.abc.javacore.introducaoclasses.classes;
+
+/**
+ *
+ * @author edivan
+ */
+// crie os seguintes atributos para essa classe;
+// nome
+// matricula
+// rg
+//cpf
+// crier uma classe de teste para preencher e imprimir os dados desse professor;
+public class Professor {
+    
+    public String nome = "";
+    public long matricular;
+    public long rg;
+    public long cpf;
+    
+    
+}
