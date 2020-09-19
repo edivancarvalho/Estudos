1be16899b4a7c435d54afae953edfedc4184ab6b
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-09-19 08:33:05 -0300

    Create ParametrosTest.java

---
+++ Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/introducaometodos/test/ParametrosTest.java
@@ -0,0 +1,22 @@
+package br.com.abc.javacore.introducaometodos.test;
+
+import br.com.abc.javacore.introducaometodos.classes.Calculadora;
+
+/**
+ *
+ * @author edivan
+ */
+public class ParametrosTest {
+    public static void main(String[] args) {
+        
+        Calculadora calculadora = new Calculadora();
+        int num1 = 5;
+        int num2 = 10;
+        calculadora.alteraDoisNumeros(num1, num2);
+        System.out.println("Dentro do ParametroTeste ");
+        System.out.println("num1: "+ num1);
+        System.out.println("num2: "+ num2);
+        
+    }
+    
+}
