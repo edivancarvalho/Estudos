434d0923135ac6778960edd9f63b57cfa7a895bd
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-09-19 11:44:50 -0300

    Update EstudanteTest.java
    Fazendo o teste com set e validando idade -1

--- Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/introducaometodos/test/EstudanteTest.java
+++ Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/introducaometodos/test/EstudanteTest.java
@@ -7,14 +7,15 @@ import br.com.abc.javacore.introducaometodos.classes.Estudante;
  * @author edivan
  */
 public class EstudanteTest {
+
     public static void main(String[] args) {
-        
+
         Estudante estudante = new Estudante();
-        estudante.nome = "Edivan";
-        estudante.idade = 30;
-        estudante.notas = new double[]{9,9,9};
+        estudante.setNome("Edivan");
+        estudante.setIdade(-1);
+        estudante.setNotas(new double[]{1, 7, 3});
         estudante.print();
         estudante.tirarMedia();
     }
-    
+
 }
