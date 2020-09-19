fd5987dad63ba0c0ca77228de07d89c6fb80cad4
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-09-19 11:44:18 -0300

    Update Estudante.java
    Mudando o atributo para private e fazendo o set e validação

--- Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/introducaometodos/classes/Estudante.java
+++ Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/introducaometodos/classes/Estudante.java
@@ -6,16 +6,16 @@ package br.com.abc.javacore.introducaometodos.classes;
  */
 public class Estudante {
 
-    public String nome;
-    public int idade;
-    public double[] notas;
+    private String nome;
+    private int idade;
+    private double[] notas;
 
     public void print() {
         System.out.println("Nome: " + this.nome);
         System.out.println("Idade: " + this.idade);
 
-        if (notas != null) {
-            for (double nota : notas) {
+        if (this.notas != null) {
+            for (double nota : this.notas) {
                 System.out.print(nota + " ");
             }
         }
@@ -24,22 +24,50 @@ public class Estudante {
     public void tirarMedia() {
         // validação de notas null no Array;
         // caso a media nao exita;
-        if (notas == null) {
+        if (this.notas == null) {
             System.out.println("esse aluuno não possui notas");
             return;
         }
 
         double media = 0;
-        for (double nota : notas) {
+        for (double nota : this.notas) {
             media += nota;
         }
-        media = media / notas.length;
+        media = media / this.notas.length;
 
         if (media > 6) {
             System.out.println("\nA média é " + media + " situação: Aprovado");
             return;
         }
         System.out.println("\nA média é " + media + " situação: Reprovado");
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        if (idade < 0) {
+            System.out.println("Você precisa de uma Idade maior ou igual a ZERO");
+            return;
+        }
+        this.idade = idade;
+    }
+
+    public void setNotas(double[] notas) {
+        this.notas = notas;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public double[] getNotas() {
+        return this.notas;
     }
 
 }
