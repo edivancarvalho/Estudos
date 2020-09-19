764af2504c18baf893df24a5aa078340f4791e40
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-09-19 10:07:41 -0300

    Update ProfessorTest.java

--- Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/introducaometodos/test/ProfessorTest.java
+++ Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/introducaometodos/test/ProfessorTest.java
@@ -11,20 +11,20 @@ public class ProfessorTest {
     public static void main(String[] args) {
 
         Professor2 prof2 = new Professor2();
-        Professor2 prof3 = new Professor2();
-
+        
         prof2.cpf = "111.111.111-11";
         prof2.matricular = "11DF";
         prof2.nome = "Edivan Carvalho";
         prof2.rg = "35673";
 
+        Professor2 prof3 = new Professor2();
         prof3.cpf = "222.111.111-21";
         prof3.matricular = "1Fsdf";
         prof3.nome = "jose Carvalho";
         prof3.rg = "35673";
 
-        prof2.imprime(prof2);
-        prof3.imprime(prof3);
+        prof2.imprime();
+        prof3.imprime();
 
     }
 
