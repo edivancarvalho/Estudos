f748ee2143fbaec74dae48a5c6344a6a945dbfa4
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-09-18 22:30:10 -0300

    classe carro

---
+++ Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/introducaoclasses/classes/Carro.java
@@ -0,0 +1,12 @@
+package br.com.abc.javacore.introducaoclasses.classes;
+
+/**
+ *
+ * @author edivan
+ */
+public class Carro {
+    public String placa;
+    public String modelo;
+    public float velocidadeMaxima;
+    
+}
