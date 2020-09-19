5e4bb2203fa23e712c2e4278d59831387c6514f9
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-09-19 08:33:07 -0300

    Create ProfessorTest.java

---
+++ Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/introducaometodos/test/ProfessorTest.java
@@ -0,0 +1,31 @@
+package br.com.abc.javacore.introducaometodos.test;
+
+import br.com.abc.javacore.introducaometodos.classes.Professor2;
+
+/**
+ *
+ * @author edivan
+ */
+public class ProfessorTest {
+
+    public static void main(String[] args) {
+
+        Professor2 prof2 = new Professor2();
+        Professor2 prof3 = new Professor2();
+
+        prof2.cpf = "111.111.111-11";
+        prof2.matricular = "11DF";
+        prof2.nome = "Edivan Carvalho";
+        prof2.rg = "35673";
+
+        prof3.cpf = "222.111.111-21";
+        prof3.matricular = "1Fsdf";
+        prof3.nome = "jose Carvalho";
+        prof3.rg = "35673";
+
+        prof2.imprime(prof2);
+        prof3.imprime(prof3);
+
+    }
+
+}
