5727bc0aee136f48f3c1e4ff28410a64a87aeb4c
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-09-18 23:44:34 -0300

    Create CalculadoraTest.java

---
+++ Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/introducaometodos/test/CalculadoraTest.java
@@ -0,0 +1,30 @@
+package br.com.abc.javacore.introducaometodos.test;
+
+import br.com.abc.javacore.introducaometodos.classes.Calculadora;
+
+/**
+ *
+ * @author edivan
+ */
+public class CalculadoraTest {
+    public static void main(String[] args) {
+        
+        Calculadora calculadora = new Calculadora();
+        
+        calculadora.somaDoisNumeros();  // metodos usa --> ();
+        calculadora.subtraiDoisNumeros();
+        calculadora.multiplicaDoisNumeros(5, 5);
+        
+        System.out.println("Divisão de dois números:");
+        
+        System.out.println(calculadora.divideDoisNumeros(20, 4));
+        double resultado = calculadora.divideDoisNumeros(20, 0);
+        System.out.println(resultado);
+        
+        // chama metodo void;
+        System.out.println("\nImprimir dois numeros");
+        calculadora.imprimeDoisNumerosDividos(20, 5);
+        System.out.println("Continuando a exec");
+    }
+    
+}
