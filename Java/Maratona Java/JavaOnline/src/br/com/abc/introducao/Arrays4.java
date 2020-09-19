f95d712b93dae875248b74b8d04902e9c8a51193
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-09-18 21:11:34 -0300

    Create Arrays4.java

---
+++ Java/Maratona Java/JavaOnline/src/br/com/abc/introducao/Arrays4.java
@@ -0,0 +1,17 @@
+package br.com.abc.introducao;
+
+/**
+ *
+ * @author edivan
+ */
+public class Arrays4 {
+    public static void main(String[] args) {
+        
+        String[] nomes = {"Edivan", "Paulo", "Joao"};
+        
+        for (String nome : nomes){
+            System.out.println(nome);
+        }
+    }
+    
+}
