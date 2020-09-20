bb8603bdc428fc6f08a27cd4d1525df4be471081
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-09-19 22:35:26 -0300

    Update Cliente.java
    Criando BLOCO DE INICIALIZAÇÃO

---
+++ Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/blocodeinicializacao/classes/Cliente.java
@@ -0,0 +1,39 @@
+package br.com.abc.javacore.blocodeinicializacao.classes;
+
+/**
+ *
+ * @author edivan
+ */
+public class Cliente {
+
+    // 1° - Alocado espaço na memoria para o objeto que será criado;
+    // 2° - cada atributo de classe é criado e inicializado com seus valores default
+    // ou valores explicitos
+    // 3° - Bloco de inicialização e executado;
+    // 4° - O contrutor é executado;
+    private int[] parcelas;
+
+    // Bloco de inicialização
+    {
+        parcelas = new int[100];
+        System.out.println("Dentro do Bloco de Inicialização");
+        for (int i = 1; i <= 100; i++){
+            parcelas[i-1] = i;
+        }
+        
+    }
+    
+    // Contrutores
+    public Cliente() {
+       
+    }
+
+    public int[] getParcela() {
+        return parcelas;
+    }
+
+    public void setParcela(int[] parcela) {
+        this.parcelas = parcela;
+    }
+
+}
