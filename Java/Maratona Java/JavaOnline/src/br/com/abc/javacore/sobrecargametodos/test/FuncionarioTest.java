aad8536e2828e412b30dd10e8a9bb1cb04f72695
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-09-19 21:00:26 -0300

    Update FuncionarioTest.java
    retirando o sobrecargas de metodos e imprimindo com os construtores

--- Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/sobrecargametodos/test/FuncionarioTest.java
+++ Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/sobrecargametodos/test/FuncionarioTest.java
@@ -10,10 +10,11 @@ public class FuncionarioTest {
 
     public static void main(String[] args) {
 
-        Funcionario funcionario = new Funcionario();
-
-        funcionario.init("Edivan Carvalho", "444.555.786-90", 5000, "345245-87");
+        Funcionario funcionario = new Funcionario("Edivan Carvalho", "444.555.786-90", 5000, "345245-87");
+        Funcionario funcionario2 = new Funcionario();
+        
         funcionario.imprime();
+        funcionario2.imprime();
     }
 
 }
