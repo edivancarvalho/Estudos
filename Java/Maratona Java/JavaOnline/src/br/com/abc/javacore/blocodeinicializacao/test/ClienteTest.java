921c428f49d23ee620ca1ca67340b1daf1e08228
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-09-19 22:35:30 -0300

    Update ClienteTest.java

--- Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/blocodeinicializacao/test/ClienteTest.java
+++ Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/blocodeinicializacao/test/ClienteTest.java
@@ -1,14 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.abc.javacore.blocodeinicializacao.test;
+
+import br.com.abc.javacore.blocodeinicializacao.classes.Cliente;
 
 /**
  *
  * @author edivan
  */
 public class ClienteTest {
-    
+
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente();
+        System.out.println("Exibindo quantidade de parcelas possiveis");
+        // Mostra um array de parcelas
+        for (int parcela : cliente.getParcela()){
+            System.out.print(parcela + " ");
+        }
+    }
+
 }
