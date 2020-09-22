159da70da85f625984f17fcb44d3d9070728ddbd
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-09-22 15:38:00 -0300

    Update Cliente.java

--- Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/Fmodificadoestatico/classes/Cliente.java
+++ Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/Fmodificadoestatico/classes/Cliente.java
@@ -5,7 +5,7 @@ package br.com.abc.javacore.Fmodificadoestatico.classes;
  * @author edivan
  */
 public class Cliente {
-    // 0° - Bloco de inicialização e executada quuando a JVM carregar a classe (Executando apenas 1 vez)
+    // 0° - Bloco de inicialização estatico é executada quuando a JVM carregar a classe (Executando apenas 1 vez)
     // 1° - Alocado espaço na memoria para o objeto que será criado;
     // 2° - cada atributo de classe é criado e inicializado com seus valores default
     // ou valores explicitos
@@ -13,8 +13,7 @@ public class Cliente {
     // 4° - O contrutor é executado;
     private static int[] parcelas;
 
-    // Bloco de inicialização
- 
+    // Bloco de inicialização 
     // com static ele somente executa somente uma vez;
     static {
         parcelas = new int[100];
@@ -22,17 +21,12 @@ public class Cliente {
         for (int i = 1; i <= 100; i++) {
             parcelas[i - 1] = i;
         }
-
     }
-
 
     // Contrutores
     public Cliente() {
-
     }
-
     public static int[] getParcelas() {
         return parcelas;
     }
-
 }
