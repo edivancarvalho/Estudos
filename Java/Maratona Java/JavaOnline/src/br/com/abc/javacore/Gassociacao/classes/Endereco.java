f2a9598efa2cdcc4049e6ab59da421e358c1f91a
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-09-22 17:47:07 -0300

    Update Endereco.java

--- Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/Gassociacao/classes/Endereco.java
+++ Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/Gassociacao/classes/Endereco.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.abc.javacore.Gassociacao.classes;
 
 /**
@@ -10,5 +5,9 @@ package br.com.abc.javacore.Gassociacao.classes;
  * @author edivan
  */
 public class Endereco {
+// Endereço tem ↓
+    private String rua;
+    private String bairro;
+    private String estado;
     
 }
