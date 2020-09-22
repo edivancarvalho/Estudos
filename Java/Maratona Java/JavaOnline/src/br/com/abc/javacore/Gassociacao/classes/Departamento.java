a721ee2325de77378634103b9448f6195b8c913a
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-09-22 17:47:04 -0300

    Update Departamento.java

--- Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/Gassociacao/classes/Departamento.java
+++ Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/Gassociacao/classes/Departamento.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.abc.javacore.Gassociacao.classes;
 
 /**
@@ -10,5 +5,6 @@ package br.com.abc.javacore.Gassociacao.classes;
  * @author edivan
  */
 public class Departamento {
-    
+    // quando o departamento tem 0 . . * de pessoas, usar se array; 
+    private Pessoa[] pessoas ;
 }
