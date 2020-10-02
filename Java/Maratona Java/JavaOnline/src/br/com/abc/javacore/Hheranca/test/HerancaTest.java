fcc47b95fdfa88edfef0dcdb7039f2028e42058a
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-10-02 05:58:25 -0300

    Create HerancaTest.java

---
+++ Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/Hheranca/test/HerancaTest.java
@@ -0,0 +1,32 @@
+package br.com.abc.javacore.Hheranca.test;
+
+import br.com.abc.javacore.Hheranca.classes.Endereco;
+import br.com.abc.javacore.Hheranca.classes.Funcionario;
+import br.com.abc.javacore.Hheranca.classes.Pessoa;
+
+/**
+ *
+ * @author edivan
+ */
+public class HerancaTest {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Edivan");
+        pessoa.setCpf("13241324123");
+        
+        Endereco end = new Endereco();
+        end.setBairro("Bairro 1");
+        end.setRua("sao pedro");
+        
+        pessoa.setEndereco(end);
+        pessoa.print();
+        
+        Funcionario fun = new Funcionario();
+        fun.setNome("Jose belo");
+        fun.setCpf("123413412");
+        fun.setEndereco(end);
+        fun.setSalario(4500.00);
+        fun.print();
+    }
+    
+}
