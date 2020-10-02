38c1777d62ea2ca6470b522d85e7b16673a4abe8
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-10-02 05:58:21 -0300

    Create Funcionario.java

---
+++ Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/Hheranca/classes/Funcionario.java
@@ -0,0 +1,21 @@
+
+package br.com.abc.javacore.Hheranca.classes;
+
+/**
+ *
+ * @author edivan
+ */
+//           Funcionario  é uma  Pessoa
+public class Funcionario extends Pessoa{
+    private double salario;
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    
+    
+}
