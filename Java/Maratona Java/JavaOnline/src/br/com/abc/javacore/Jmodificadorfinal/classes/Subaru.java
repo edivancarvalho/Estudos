687376e7d340300ba2505c2a4c8e22f1b91e7bf9
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-10-03 12:04:10 -0300

    Create Subaru.java

---
+++ Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/Jmodificadorfinal/classes/Subaru.java
@@ -0,0 +1,15 @@
+package br.com.abc.javacore.Jmodificadorfinal.classes;
+
+/**
+ *
+ * @author edivan
+ */
+public class Subaru extends Carro {
+
+
+    /* A class Subaru nao pode ser extend de carro
+    quando a class Carro for FINAL*/
+    public void print(String nome) {
+
+    }
+}
