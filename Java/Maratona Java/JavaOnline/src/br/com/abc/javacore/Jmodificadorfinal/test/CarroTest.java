752c6c502742dd3b6e72af1b14c4761cfe1304a0
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-10-03 12:02:03 -0300

    Create CarroTest.java

---
+++ Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/Jmodificadorfinal/test/CarroTest.java
@@ -0,0 +1,21 @@
+package br.com.abc.javacore.Jmodificadorfinal.test;
+
+import br.com.abc.javacore.Jmodificadorfinal.classes.Carro;
+
+/**
+ *
+ * @author edivan
+ */
+public class CarroTest {
+    public static void main(String[] args) {
+        Carro c = new Carro();
+        System.out.println(c.getComprador());
+        
+        c.getComprador().setNome("Kuririn");
+        System.out.println(c.getComprador());
+        
+        
+        
+    }
+    
+}
