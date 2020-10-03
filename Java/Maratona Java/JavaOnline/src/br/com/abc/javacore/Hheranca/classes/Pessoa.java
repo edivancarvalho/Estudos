d6a04bb1a2b2aefc9a03cdb96e251261a9ceac31
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-10-03 10:17:07 -0300

    Update Pessoa.java
    Usando bloco de inicialização

--- Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/Hheranca/classes/Pessoa.java
+++ Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/Hheranca/classes/Pessoa.java
@@ -5,9 +5,27 @@ package br.com.abc.javacore.Hheranca.classes;
  * @author edivan
  */
 public class Pessoa {
+
     protected String nome;
-    protected String cpf;    
+    protected String cpf;
     protected Endereco endereco;
+
+    public Pessoa(String nome) {
+        System.out.println("Detro do construtor pessoa");
+        this.nome = nome;
+    }
+
+    static {
+        System.out.println("Bloco de inicialição estatico pessoa");
+    }
+
+    {
+        System.out.println("Bloco de inicializao de pessoa 1");
+    }
+
+    {
+        System.out.println("Bloco de inicializao de pessoa 2");
+    }
 
     public String getNome() {
         return nome;
@@ -32,10 +50,10 @@ public class Pessoa {
     public void setEndereco(Endereco endereco) {
         this.endereco = endereco;
     }
-    
-    public void print(){
-        System.out.println("Nome: "+ this.nome);
-        System.out.println("CPF: "+ this.cpf);
-        System.out.println("End: "+ this.endereco.getBairro());
+
+    public void print() {
+        System.out.println("Nome: " + this.nome);
+        System.out.println("CPF: " + this.cpf);
+        System.out.println("End: " + this.endereco.getBairro());
     }
 }
