f87edcd83cde655afa1d71205958a680b00c88d4
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-10-03 10:17:12 -0300

    Update HerancaTest.java
    Usando bloco de inicialização

--- Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/Hheranca/test/HerancaTest.java
+++ Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/Hheranca/test/HerancaTest.java
@@ -10,26 +10,26 @@ import br.com.abc.javacore.Hheranca.classes.Pessoa;
  */
 public class HerancaTest {
     public static void main(String[] args) {
-        Pessoa pessoa = new Pessoa();
-        pessoa.setNome("Edivan");
-        pessoa.setCpf("13241324123");
+        //Pessoa pessoa = new Pessoa("Edivan");
+        //pessoa.setNome("Edivan");
+       // pessoa.setCpf("13241324123");
         
         Endereco end = new Endereco();
-        end.setBairro("Bairro 1");
+        //end.setBairro("Bairro 1");
         end.setRua("sao pedro");
         
         
-        pessoa.setEndereco(end);
-        pessoa.print();
+        //pessoa.setEndereco(end);
+        //pessoa.print();
         System.out.println("----------------");
         
-        Funcionario fun = new Funcionario();
-        fun.setNome("Jose belo");
+        Funcionario fun = new Funcionario("Jose belo");
+        //fun.setNome("Jose belo");
         fun.setCpf("123413412");
         fun.setSalario(4500.00);
         fun.setEndereco(end);
         
-        fun.print();
+       // fun.print();
     }
     
 }
