1e6837e466544d8103716ca2a2d328ee34a7669c
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-10-03 22:22:51 -0300

    Update TipoCliente.java

--- Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/Kenum/classes/TipoCliente.java
+++ Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/Kenum/classes/TipoCliente.java
@@ -5,6 +5,16 @@ package br.com.abc.javacore.Kenum.classes;
  * @author edivan
  */
 public enum TipoCliente {
-    PESSOA_FISICA, PESSOA_JURIDICA;
+    PESSOA_FISICA(1), PESSOA_JURIDICA(2);
+
+    private int tipo;
+
+    private TipoCliente(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
     
 }
