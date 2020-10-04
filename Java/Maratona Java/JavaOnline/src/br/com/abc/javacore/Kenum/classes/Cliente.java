87c28ca37b38ca79de5e9dbf0a8cd6c1ba9f1873
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-10-03 21:56:21 -0300

    Create Cliente.java

---
+++ Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/Kenum/classes/Cliente.java
@@ -0,0 +1,42 @@
+package br.com.abc.javacore.Kenum.classes;
+
+/**
+ *
+ * @author edivan
+ */
+public class Cliente {
+
+    private String nome;    
+    private TipoCliente tipo;
+
+    public Cliente(String nome, TipoCliente tipo) {
+        this.nome = nome;
+        this.tipo = tipo;
+    }
+    
+    
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public TipoCliente getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoCliente tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "nome=" + nome + ", tipo=" + tipo + '}';
+    }
+    
+    
+
+}
