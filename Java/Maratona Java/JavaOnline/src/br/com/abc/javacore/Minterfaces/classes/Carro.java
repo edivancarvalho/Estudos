e9cbe472496ffc095b37aa9557ed0f3486a3ccee
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-10-10 18:56:36 -0300

    Update Carro.java

--- Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/Minterfaces/classes/Carro.java
+++ Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/Minterfaces/classes/Carro.java
@@ -5,7 +5,7 @@ package br.com.abc.javacore.Minterfaces.classes;
  * @author edivan
  */
 public class Carro  implements Tributavel{
-
+    // private > default, protected >  public
     @Override
     public void calculaImposto() {
         
