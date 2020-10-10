1ea7bb82f195a3b390718c0d6dd16d1d15b56c27
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-10-10 18:56:41 -0300

    Update Tributavel.java

--- Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/Minterfaces/classes/Tributavel.java
+++ Java/Maratona Java/JavaOnline/src/br/com/abc/javacore/Minterfaces/classes/Tributavel.java
@@ -6,5 +6,5 @@ package br.com.abc.javacore.Minterfaces.classes;
  */
 public interface Tributavel {
     public static final double IMPOSTO = 0.2;
-    public abstract void calculaImposto();
+    void calculaImposto();
 }
