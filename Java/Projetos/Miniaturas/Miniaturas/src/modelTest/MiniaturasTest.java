9aacba18a5d424fad89d8629d864db58ddfafb32
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-10-12 02:01:18 -0300

    Update MiniaturasTest.java

--- Java/Projetos/Miniaturas/Miniaturas/src/modelTest/MiniaturasTest.java
+++ Java/Projetos/Miniaturas/Miniaturas/src/modelTest/MiniaturasTest.java
@@ -4,7 +4,7 @@ import model.Fabricantes;
 import model.Fotos;
 import model.Miniaturas;
 import model.Temas;
-import model.Tipo_miniaturas;
+import model.Tipo_Miniaturas;
 
 /**
  *
@@ -13,13 +13,13 @@ import model.Tipo_miniaturas;
 public class MiniaturasTest {
 
     public static void main(String[] args) {
-        Miniaturas miniaturas = new Miniaturas(1, "2000", "2020", "Em otimos estado", "Unica", "nove", 90.00);
+        Miniaturas miniaturas = new Miniaturas(1, "2000", "2020", "Em otimos estado", "Unica", "nove", "90.00");
         
         Fabricantes fabricantes = new Fabricantes(1, "ford");
         Fabricantes fabricantes1 = new Fabricantes(2, "Gol");
 
-        Tipo_miniaturas tipo_miniaturas = new Tipo_miniaturas(1, "Medio");
-        Tipo_miniaturas tipo_miniaturas2 = new Tipo_miniaturas(2, "Medio porte");
+        Tipo_Miniaturas tipo_miniaturas = new Tipo_Miniaturas(1, "Medio");
+        Tipo_Miniaturas tipo_miniaturas2 = new Tipo_Miniaturas(2, "Medio porte");
         
         Temas temas = new Temas(1, "Ouro");
         
