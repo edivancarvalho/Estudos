a91e3a25acbad3cd3f6dcc0e315105d87edf7407
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-10-12 01:59:21 -0300

    Create Tipo_Miniatura.java

---
+++ Java/Projetos/Miniaturas/Miniaturas/src/bll/Tipo_Miniatura.java
@@ -0,0 +1,45 @@
+package bll;
+
+import dao.Tipo_MiniaturaDao;
+import java.util.List;
+import model.Tipo_Miniaturas;
+
+/**
+ *
+ * @author edivan
+ */
+public class Tipo_Miniatura {
+    
+    // chama a classe FabricanteDAo
+    private Tipo_MiniaturaDao fabDao;
+
+    // Construtor recebendo a conexao atravez da fabDao pelo objeto fabricanteDao;
+    public Tipo_Miniatura() {
+        fabDao = new Tipo_MiniaturaDao();
+    }
+
+    public void adicionarTipo_Miniatura(Tipo_Miniaturas objtipo_miniaturas) throws Exception {
+        fabDao.addTipo_Miniatura(objtipo_miniaturas);
+
+    }
+
+    public void removerTipo_Miniatura(Tipo_Miniaturas objtipo_miniaturas) throws Exception {
+        fabDao.deleteTipo_Miniatura(objtipo_miniaturas.getIden());
+
+    }
+
+    public void alterarTipo_Miniatura(Tipo_Miniaturas objtipo_miniaturas) throws Exception {
+        fabDao.updateTipo_Miniatura(objtipo_miniaturas);
+
+    }
+
+    public List<Tipo_Miniaturas> consultarTipo_Miniaturas() throws Exception {
+        return fabDao.getAllTipo_miniatura();
+    }
+
+    public Tipo_Miniaturas consultaTipo_MiniaturasPorId(int id) throws Exception {
+        return fabDao.getTipo_miniaturaById(id);
+    }
+
+    // fazer validação
+}
