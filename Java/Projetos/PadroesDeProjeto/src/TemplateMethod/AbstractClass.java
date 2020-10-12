a9c774a1921564cfd6adf4bedbe5542d4b0f8a07
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-10-12 01:56:11 -0300

    Update AbstractClass.java

--- Java/Projetos/PadroesDeProjeto/src/TemplateMethod/AbstractClass.java
+++ Java/Projetos/PadroesDeProjeto/src/TemplateMethod/AbstractClass.java
@@ -4,23 +4,17 @@ package TemplateMethod;
  *
  * @author edivan
  */
-public abstract class AbstractClass { 
-    
+public abstract class AbstractClass {
 
     public void templateMethod() {
-        
 
         // o templateMethod usar os metodos ↓ operacao1 e 2;
         primitiveOperacao1();
         primitiveOperacao2();
-        
-        
     }
 // operação
 
-    
     public abstract void primitiveOperacao1();
 
-    public abstract void primitiveOperacao2();
-
+   public abstract void primitiveOperacao2();
 }
