7f0f9bff54c93000a65aff192ad048373c5ff1d0
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-10-21 10:15:17 -0300

    Create atividade01.java

---
+++ BootCamp/src/aAtividades/atividade01.java
@@ -0,0 +1,17 @@
+package aAtividades;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author edivan
+ */
+public class atividade01 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        
+    }
+
+}
