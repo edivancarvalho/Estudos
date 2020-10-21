b94090bbdc01ae76eb35bd815007e936882e5437
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-10-21 07:40:47 -0300

    Create ExemploIf.java

---
+++ BootCamp/src/modelos/ExemploIf.java
@@ -0,0 +1,21 @@
+package modelos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author edivan
+ */
+public class ExemploIf {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Digite a idade:");
+        int idade = scan.nextInt();
+        if (idade >= 16) {
+            System.out.println("Pode votar");
+        } else {
+            System.out.println("Não pode votar");
+        }
+    }
+}
