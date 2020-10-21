416eb6be940db8b7268859765534a3b4ae21e99a
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-10-21 08:49:00 -0300

    Create ExemploDo.java

---
+++ BootCamp/src/CmodelosRepaticao/ExemploDo.java
@@ -0,0 +1,19 @@
+package CmodelosRepaticao;
+
+/**
+ *
+ * @author edivan
+ */
+public class ExemploDo {
+
+    public static void main(String[] args) {
+//        for (int i = 0; i < 15; i++) {
+//            System.out.printf("\nlinha: %d", i);
+//        }
+        int i = 0;
+        do {
+            System.out.printf("\nlinha: %d", i);
+            i++;
+        } while (i < 15);
+    }
+}
