a8830cc0163e99874baac23bdbe9dc0880859a45
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-10-21 07:40:20 -0300

    Create ExemploCondicional.java

---
+++ BootCamp/src/modelos/ExemploCondicional.java
@@ -0,0 +1,18 @@
+package modelos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author edivan
+ */
+public class ExemploCondicional {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Digite a nota:");
+        int nota = scan.nextInt();
+        System.out.printf("\n%s %s", "O aluno foi", (nota >= 5 ? "aprovado"
+                : "reprovado"));
+    }
+}
