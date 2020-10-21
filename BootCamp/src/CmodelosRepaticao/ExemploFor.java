2840a31203ad54e030d6bea55f826e9434b370aa
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-10-21 08:24:25 -0300

    Create ExemploFor.java

---
+++ BootCamp/src/CmodelosRepaticao/ExemploFor.java
@@ -0,0 +1,13 @@
+package CmodelosRepaticao;
+
+/**
+ *
+ * @author edivan
+ */
+public class ExemploFor {
+    public static void main(String[] args) {
+		for (int i = 0; i < 15; i++) {
+			System.out.printf("\nlinha: %d", i);
+		}
+	}
+}
