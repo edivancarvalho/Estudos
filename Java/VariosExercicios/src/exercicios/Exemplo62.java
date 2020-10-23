26dcf2fafc93e34b7d3c0a2f646913da370675b0
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-10-23 10:57:11 -0300

    Update Exemplo62.java
    //algoritimo que efetuar a leitura de 10 elementos interios de um vetor Teste01. Vamos contruir um vetor teste02 do mesmo tipo, observando a seguinte regra de formação: se o valor do indice for PAR, o valor do elemneto deverá ser multiplicado por 5, se for IMPAR deverá ser somando por 5. ao final o conteudo dos dois vetores.

--- Java/VariosExercicios/src/exercicios/Exemplo62.java
+++ Java/VariosExercicios/src/exercicios/Exemplo62.java
@@ -1,7 +1,7 @@
 package exercicios;
 
 import java.util.Scanner;
-
+//algoritimo que efetuar a leitura de 10 elementos interios de um vetor Teste01. Vamos contruir um vetor teste02 do mesmo tipo, observando a seguinte regra de formação: se o valor do indice for PAR, o valor do elemneto deverá ser multiplicado por 5, se for IMPAR deverá ser somando por 5. ao final o conteudo dos dois vetores.
 /**
  *
  * @author edivan
@@ -30,7 +30,7 @@ public class Exemplo62 {
             
             for (int i = 0; i < 10; i++) {
                 System.out.print("teste01[" + i + "]= " + teste01[i] + "\t");
-                System.out.println("teste02[" + i + "] = " + teste01[i]);
+                System.out.println("teste02[" + i + "] = " + teste02[i]);
             }
         } catch (Exception e) {
             System.out.println("ocorreu um erro durante a leitura!2");
